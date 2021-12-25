aefc27282f11fb74b6ed0f19994b6e55425a7752
thepeaknick <basta@peaknick.com>
2021-12-25 13:25:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from werkzeug.wrappers import Request, Response
+from werkzeug.utils import secure_filename
+
+home_team_global = ''
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/teamlogo', methods = ['POST'])
+def addTeamLogo():
+    file = request.files['file']
+    # Read the image via file.stream
+    file.save('teamLogos/' + secure_filename(file.filename))
+
+    return jsonify({'msg': 'success'})
+
+@app.route('/api/v1.0/teamandplayers', methods = ['POST'])
+def teamandplayers():
+    home_team_global = request.get_json()
+    
+    return jsonify({'msg': 'success'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
