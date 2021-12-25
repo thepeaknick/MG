dd2e83fae6fd01968f7d621dc5ba6c2cf60de544
thepeaknick <basta@peaknick.com>
2021-12-25 13:32:18 +0100

    Added PIL

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ from flask import Flask, request, jsonify
 from werkzeug.wrappers import Request, Response
 from werkzeug.utils import secure_filename
 
+import sys
+if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
+    import Tkinter
+    tkinter = Tkinter #I decided to use a library reference to avoid potential naming conflicts with people's programs.
+else:
+    import tkinter
+from PIL import Image, ImageTk
+
+def showPIL(pilImage):
+    root = tkinter.Tk()
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.overrideredirect(1)
+    root.geometry("%dx%d+0+0" % (w, h))
+    root.focus_set()    
+    root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
+    canvas = tkinter.Canvas(root,width=w,height=h)
+    canvas.pack()
+    canvas.configure(background='black')
+    imgWidth, imgHeight = pilImage.size
+    if imgWidth > w or imgHeight > h:
+        ratio = min(w/imgWidth, h/imgHeight)
+        imgWidth = int(imgWidth*ratio)
+        imgHeight = int(imgHeight*ratio)
+        pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(pilImage)
+    imagesprite = canvas.create_image(w/2,h/2,image=image)
+    root.mainloop()
+
+
 home_team_global = ''
 away_team_global = ''
 
@@ -24,3 +53,6 @@ def teamandplayers():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+pilImage = Image.open("/scoreboard/background.png")
+showPIL(pilImage)
